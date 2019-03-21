33691cbf3c766887d03cebe5ce8efb818df7887d
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-21 18:31:48 +0300

    create registration form and view

---
+++ watch/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from .models import *
+
+
+class Registration(UserCreationForm):
+   '''
+   User registration form.
+   '''
+
+   class Meta(UserCreationForm.Meta):
+      model = User
+      fields = UserCreationForm.Meta.fields + ('first_name','last_name','email','username','password')
--- watch/views.py
+++ watch/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth import login,authenticate,logout
+from .models import *
+from .forms import *
 
 # Create your views here.
 def landing(request):
 
    return render(request, 'watch/landing.html')
+
+def register(request):
+
+   title = 'Sign Up'
+
+   if request.method == 'POST':
+      form = Registration(request.POST)
+      if form.is_valid():
+         form.save()
+         username = form.cleaned_data.get('username')
+         raw_password = form.cleaned_data.get('password')
+         user = authenticate(username=username,password=raw_password)
+         login(request,user)
+         profile = Profile(user=user)
+         profile.save()
+         return redirect('landing')
+   else:
+      form = Registration()
+
+   context = {
+      'title': title,
+      'form': form
+   }
+
+   return render(request, 'registration/register.html')
