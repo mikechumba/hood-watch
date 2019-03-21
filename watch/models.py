492f9e3d7a076dd1caae2deffd25dc6face2feb0
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-21 08:22:45 +0300

    create a business model

--- watch/models.py
+++ watch/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Neighbourhood(models.Model):
+   '''
+   Holds neighbourhood data
+   '''
+
+   name = models.CharField(max_length=50)
+   location = models.CharField(max_length=50)
+   admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+   def __str__(self):
+      return self.name
+
+class Profile(models.Model):
+   '''
+   Holds user's profile data.
+   '''
+
+
+   user = models.OneToOneField(User, on_delete=models.CASCADE)
+   avatar = models.ImageField(upload_to='avatars/')
+   neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+
+   def __str__(self):
+      return self.user.username
+
+
+class Business(models.Model):
+   '''
+   Holds info on businesses in each neighbourhood
+   '''
+
+   name = models.CharField(max_length=50)
+   email = models.EmailField(max_length=254)
+   hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+   category = models.ForeignKey(Category,)
+
+   def __str__(self):
+      return self.name
+
+class HealthCenter(models.Model):
+   '''
+
+   '''
